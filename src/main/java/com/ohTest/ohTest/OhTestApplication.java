6986035c4187d3878da85c588a5105cdbfcf4515
oh-namgyu <nkoh@erns.co.kr>
2022-01-18 21:44:13 +0900

    초기설정파일
    초기 설정

---
+++ src/main/java/com/ohTest/ohTest/OhTestApplication.java
@@ -0,0 +1,13 @@
+package com.ohTest.ohTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohTest/ohTest/OhTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohTest.ohTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
